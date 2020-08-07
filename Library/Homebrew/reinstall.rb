raffermeko/brew

commit 709ea2f9985a4ac99b0b50d614e5a208f74d5464

Pass `verbose?` to `Keg#link`, `Keg#unlink` and `Keg#optlink`.

Library/Homebrew/reinstall.rb
@@ -49,7 +49,7 @@ module Homebrew
   rescue FormulaInstallationAlreadyAttemptedError
     nil
   rescue Exception # rubocop:disable Lint/RescueException
-    ignore_interrupts { restore_backup(keg, keg_was_linked) }
+    ignore_interrupts { restore_backup(keg, keg_was_linked, verbose: args.verbose?) }
     raise
   else
     begin
@@ -82,7 +82,7 @@ module Homebrew
     Pathname.new(keg).rmtree if keg.exist?
 
     path.rename keg
-    keg.link if keg_was_linked
+    keg.link(verbose: verbose) if keg_was_linked
   end
 
   def backup_path(path)

Library/Homebrew/cmd/reinstall.rb
@@ -67,7 +67,7 @@ module Homebrew
         onoe "#{f.full_name} is pinned. You must unpin it to reinstall."
         next
       end
-      Migrator.migrate_if_needed(f, force: args.force?)
+      Migrator.migrate_if_needed(f, force: args.force?, verbose: args.verbose?)
       reinstall_formula(f, args: args)
       Cleanup.install_formula_clean!(f)
     end

Library/Homebrew/build.rb
@@ -201,7 +201,7 @@ class Build
     else
       raise
     end
-    Keg.new(path).optlink
+    Keg.new(path).optlink(verbose: args.verbose?)
   rescue
     raise "#{f.opt_prefix} not present or broken\nPlease reinstall #{f.full_name}. Sorry :("
   end

Library/Homebrew/cmd/install.rb
@@ -258,7 +258,7 @@ module Homebrew
     Install.perform_preinstall_checks(cc: args.cc)
 
     formulae.each do |f|
-      Migrator.migrate_if_needed(f, force: args.force?)
+      Migrator.migrate_if_needed(f, force: args.force?, verbose: args.verbose?)
       install_formula(f, args: args)
       Cleanup.install_formula_clean!(f)
     end

Library/Homebrew/cmd/link.rb
@@ -31,10 +31,11 @@ module Homebrew
   def link
     args = link_args.parse
 
-    mode = OpenStruct.new
-
-    mode.overwrite = true if args.overwrite?
-    mode.dry_run = true if args.dry_run?
+    options = {
+      overwrite: args.overwrite?,
+      dry_run:   args.dry_run?,
+      verbose:   args.verbose?,
+    }
 
     args.kegs.each do |keg|
       keg_only = Formulary.keg_only?(keg.rack)
@@ -53,13 +54,13 @@ module Homebrew
         next
       end
 
-      if mode.dry_run
-        if mode.overwrite
+      if args.dry_run?
+        if args.overwrite?
           puts "Would remove:"
         else
           puts "Would link:"
         end
-        keg.link(mode)
+        keg.link(**options)
         puts_keg_only_path_message(keg) if keg_only
         next
       end
@@ -89,7 +90,7 @@ module Homebrew
         puts if args.verbose?
 
         begin
-          n = keg.link(mode)
+          n = keg.link(**options)
         rescue Keg::LinkError
           puts
           raise

Library/Homebrew/cmd/migrate.rb
@@ -33,7 +33,7 @@ module Homebrew
         raise "#{rack} is a symlink" if rack.symlink?
       end
 
-      migrator = Migrator.new(f, force: args.force?)
+      migrator = Migrator.new(f, force: args.force?, verbose: args.verbose?)
       migrator.migrate
     end
   end

Library/Homebrew/cmd/switch.rb
@@ -49,7 +49,7 @@ module Homebrew
 
     # Link new version, if not keg-only
     if Formulary.keg_only?(rack)
-      keg.optlink
+      keg.optlink(verbose: args.verbose?)
       puts "Opt link created for #{keg}"
     else
       puts "#{keg.link} links created for #{keg}"

Library/Homebrew/cmd/unlink.rb
@@ -26,20 +26,19 @@ module Homebrew
   def unlink
     args = unlink_args.parse
 
-    mode = OpenStruct.new
-    mode.dry_run = true if args.dry_run?
+    options = { dry_run: args.dry_run?, verbose: args.verbose? }
 
     args.kegs.each do |keg|
-      if mode.dry_run
+      if args.dry_run?
         puts "Would remove:"
-        keg.unlink(mode)
+        keg.unlink(**options)
         next
       end
 
       keg.lock do
         print "Unlinking #{keg}... "
         puts if args.verbose?
-        puts "#{keg.unlink(mode)} symlinks removed"
+        puts "#{keg.unlink(**options)} symlinks removed"
       end
     end
   end

commit 98f13263c011d0883f25c9ab95062be094607951

Fix “wrong number of arguments” error
This commit fixes an error introduced in 709ea2f998, which would
surface on certain install/reinstall/upgrade failures.

Example:

```
==> Patching
patching file default_out.txt
Hunk #2 FAILED at 11.
1 out of 2 hunks FAILED -- saving rejects to file default_out.txt.rej
Error: wrong number of arguments (given 3, expected 2)
/usr/local/Homebrew/Library/Homebrew/reinstall.rb:77:in `restore_backup'
/usr/local/Homebrew/Library/Homebrew/reinstall.rb:52:in `block in reinstall_formula'
/usr/local/Homebrew/Library/Homebrew/utils.rb:377:in `ignore_interrupts'
/usr/local/Homebrew/Library/Homebrew/reinstall.rb:52:in `rescue in reinstall_formula'
/usr/local/Homebrew/Library/Homebrew/reinstall.rb:10:in `reinstall_formula'
/usr/local/Homebrew/Library/Homebrew/cmd/reinstall.rb:71:in `block in reinstall'
/usr/local/Homebrew/Library/Homebrew/cmd/reinstall.rb:65:in `each'
/usr/local/Homebrew/Library/Homebrew/cmd/reinstall.rb:65:in `reinstall'
/usr/local/Homebrew/Library/Homebrew/brew.rb:112:in `<main>'
```

Library/Homebrew/reinstall.rb
@@ -74,7 +74,7 @@ module Homebrew
     end
   end
 
-  def restore_backup(keg, keg_was_linked)
+  def restore_backup(keg, keg_was_linked, verbose:)
     path = backup_path(keg)
 
     return unless path.directory?