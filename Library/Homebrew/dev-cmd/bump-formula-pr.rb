raffermeko/brew

commit a521859566edd685b9101f4b8cfdf985336ce31a

dev-cmd/bump-formula-pr: fix for undetectable version
Currently, `brew bump-formula-pr` errors out without a proper message
when `--version` isn't specified and the formula's version cannot be parsed
from the URL. We fix this by returning early from `check_new_version` when the
detected version is Version::NULL.

Library/Homebrew/dev-cmd/bump-formula-pr.rb
@@ -436,6 +436,9 @@ module Homebrew
       specs[:tag] = tag if tag.present?
       version = Version.detect(url, **specs)
     end
+
+    return if version.null?
+
     check_throttle(formula, version)
     check_closed_pull_requests(formula, tap_remote_repo, args: args, version: version)
   end

commit 953a1fdd6917a50ad92ea2958151ff024611accd

dev-cmd/bump-formula-pr: fix undefined method null error
Fix a regression from a521859566edd685b9101f4b8cfdf985336ce31a.
Only if `version.nil?` is `Version.detect` used, so we can
only use `version.null?` after that. (Otherwise, it's a string.)

```
Error: undefined method `null?' for "X.Y.Z":String
Please report this issue:
  https://docs.brew.sh/Troubleshooting
/usr/local/Homebrew/Library/Homebrew/dev-cmd/bump-formula-pr.rb:440:in `check_new_version'
/usr/local/Homebrew/Library/Homebrew/dev-cmd/bump-formula-pr.rb:131:in `bump_formula_pr'
/usr/local/Homebrew/Library/Homebrew/brew.rb:110:in `<main>'
```

Library/Homebrew/dev-cmd/bump-formula-pr.rb
@@ -435,9 +435,8 @@ module Homebrew
       specs = {}
       specs[:tag] = tag if tag.present?
       version = Version.detect(url, **specs)
+      return if version.null?
     end
-
-    return if version.null?
 
     check_throttle(formula, version)
     check_closed_pull_requests(formula, tap_remote_repo, args: args, version: version)